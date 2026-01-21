e370fa0b0a64abd2657ab72197a193e15499adac
nickiguess <nick.power1000@gmail.com>
2026-01-21 11:04:07 +0100

    Creato progetto Java Spring Boot

---
+++ EduQuest/src/test/java/dev/eduteam/eduquest/EduQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eduteam.eduquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
