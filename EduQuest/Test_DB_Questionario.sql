90d4ee591b6e237695204f3bf4ae196dcd7256bd
nickiguess <nick.power1000@gmail.com>
2026-01-24 16:52:30 +0100

    Aggiornato file SQL con Insert

--- EduQuest/Test_DB_Questionario.sql
+++ EduQuest/Test_DB_Questionario.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE Test_Questionario;
 USE Test_Questionario;
 
-CREATE TABLE questionario(
+CREATE TABLE questionari(
 	
     questionarioID INTEGER auto_increment,
     nome VARCHAR(40) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE questionario(
     PRIMARY KEY(questionarioID)
 ) ENGINE=INNODB;
 
-CREATE TABLE domanda(
+CREATE TABLE domande(
 
 	domandaID INTEGER auto_increment,
     testo VARCHAR(500) NOT NULL,
@@ -22,13 +22,13 @@ CREATE TABLE domanda(
 	CONSTRAINT PK_Domanda PRIMARY KEY (domandaID,questionario),
     
     CONSTRAINT FK_QuestionarioDiOrigine FOREIGN KEY (questionario)
-    REFERENCES questionario(questionarioID)
+    REFERENCES questionari(questionarioID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
     
 ) ENGINE=INNODB;
 
-CREATE TABLE risposta(
+CREATE TABLE risposte(
 
 	rispostaID INTEGER auto_increment,
     testo VARCHAR(500) NOT NULL,
@@ -37,9 +37,14 @@ CREATE TABLE risposta(
 	CONSTRAINT PK_Risposta PRIMARY KEY (rispostaID,domanda),
 
     CONSTRAINT FK_DomandaDiOrigine FOREIGN KEY (domanda)
-    REFERENCES domanda(domandaID)
+    REFERENCES domande(domandaID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 ) ENGINE=INNODB;
 
-ALTER TABLE domanda ADD CONSTRAINT FK_RispostaCorretta FOREIGN KEY (rispostaCorretta) REFERENCES risposta(rispostaID) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE domande ADD CONSTRAINT FK_RispostaCorretta FOREIGN KEY (rispostaCorretta) REFERENCES risposte(rispostaID) ON DELETE SET NULL ON UPDATE CASCADE;
+
+INSERT INTO questionari (nome, descrizione, numeroDomande, dataCreazione) VALUES
+('Matematica', 'Prova di matematica', '5', '2001-09-11'),
+('Italiano', 'Prova di italiano', '3', '2004-06-05'),
+('Simbolismo', 'La nebbia agli irti colli piovigginando sale', '1', '2026-01-24');
