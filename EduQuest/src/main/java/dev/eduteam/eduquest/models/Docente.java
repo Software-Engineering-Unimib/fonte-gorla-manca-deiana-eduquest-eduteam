60be0205193e3ddd49496eefeda3b821a2c47fad
Giovanni Deiana <g.deiana1@campus.unimib.it>
2026-01-24 18:01:21 +0100

    aggiunta media a studente e insegnamento a docente

--- EduQuest/src/main/java/dev/eduteam/eduquest/models/Docente.java
+++ EduQuest/src/main/java/dev/eduteam/eduquest/models/Docente.java
@@ -2,12 +2,24 @@ package dev.eduteam.eduquest.models;
 
 public class Docente extends Account {
 
+    private String insegnamento;
+
     public Docente() {
         super();
+        this.insegnamento = null;
     }
 
     public Docente(String nome, String cognome, String userName, String email, String password) {
         super(nome, cognome, userName, email, password);
+        this.insegnamento = null;
+    }
+
+    public String getInsegnamento() {
+        return insegnamento;
+    }
+
+    public void setInsegnamento(String insegnamento) {
+        this.insegnamento = insegnamento;
     }
 
     @Override
--- EduQuest/src/main/java/dev/eduteam/eduquest/models/Studente.java
+++ EduQuest/src/main/java/dev/eduteam/eduquest/models/Studente.java
@@ -2,12 +2,24 @@ package dev.eduteam.eduquest.models;
 
 public class Studente extends Account {
 
+    private double mediaPunteggio;
+
     public Studente() {
         super();
+        this.mediaPunteggio = 0.0;
     }
 
     public Studente(String nome, String cognome, String userName, String email, String password) {
         super(nome, cognome, userName, email, password);
+        this.mediaPunteggio = 0.0;
+    }
+
+    public double getMediaPunteggio() {
+        return mediaPunteggio;
+    }
+
+    public void setMediaPunteggio(double mediaPunteggio) {
+        this.mediaPunteggio = mediaPunteggio;
     }
 
     @Override
